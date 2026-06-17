a83b8dd92e4b06eed8b11a26011dd113c5c54b66
Akrathod007 <anshrathod2104@gmail.com>
2026-06-17 14:39:02 +0530

    File Added

---
+++ file.py
@@ -0,0 +1,93 @@
+# f = open("Student.txt", "r")
+# print(f.read())
+# f.close()
+
+# f = open("Student.txt", "w")
+# f.write("Hello")
+# f.close()
+
+# f = open("Student.txt", "a")
+# f.write("\nWorld")
+# f.close()
+
+# f = open("Student1.txt", "x")
+# f.close()
+
+# f = open("Student.txt", "r")
+# data = f.read()
+# data = f.read(10)
+# print(data)
+# print(f.readline())
+# print(f.readline())
+# data = f.readlines()
+# print(data)
+# f.close()
+
+# f = open("Student.txt", "w")
+# f.write("Hello\n")
+# f.write("World\n")
+# f.write("Bye\n")
+# lines = ["Ram\n", "Shyam\n", "Mohan\n"]
+# f.writelines(lines)
+# f.close()
+
+# with open("Student.txt", "r") as f:
+#     data = f.read()
+#     print(data)
+
+
+# f = open("Student.txt", "r")
+# print(f.tell())
+# f.read(7)
+# print(f.tell())
+# f.close()
+
+
+# import os
+
+# if os.path.exists("Student1.txt"):
+#     print("File Exists")
+# else:
+#     print("File Not Found")
+
+# os.remove("student.txt")
+
+# os.rename("Student1.txt", "Student.txt")
+
+# f = open("Demo.txt", "w")
+# f.write("Hello")
+# f.close()
+
+# try:
+#     f = open("Demo.txt", "r")
+#     print(f.read())
+
+# except FileNotFoundError:
+#     print("File Not Found")
+
+# oldFile = open("Demo.txt", "r")
+# data = oldFile.read()
+
+# newFile = open("./File/NewFile.txt", "w")
+# newFile = open("C:/Users/Ansh/Desktop/Hello.txt", "w")
+# newFile.write("Hello")
+
+# oldFile.close()
+# newFile.close()
+
+# f = open("image1.jpeg", "rb")
+# data = f.read()
+# print(data)
+# f.close()
+
+# f2 = open("bheem2.png", "wb")
+# f2.write(data)
+
+f = open("audio2.mp3", "rb")
+data = f.read()
+print(data)
+f.close()
+
+f2 = open("song.mp3", "wb")
+f2.write(data)
+f2.close()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-__mark
+Student.txt
